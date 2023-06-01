b91cb113d076f4e0b27db4383c946d6e22b5c9d6
kalpemo <alberto.pena@udea.edu.co>
2023-06-01 04:15:31 +0000

    UBServer-commit CAPM 9: actualizando iozone

--- Bonnie/md0/bonnie_script.py
+++ Bonnie/md0/bonnie_script.py
@@ -1,25 +1,8 @@
 import subprocess
 
-def run_bonnie_raid0():
-    raid_device = subprocess.getoutput("sudo lsblk -o NAME,MOUNTPOINT | grep md0 | awk '{print $2}'")
-    benchmark_output = 'bonnie_results.txt'
+output_file = "raid@raid:~/repo/RAID4UdeAServer/Bonnie/md0/bonnie_results.csv"
 
-    # Construir el comando para ejecutar Bonnie++ en el RAID 0
-    command = f'sudo bonnie++ -d {raid_device} -s 1G -u 1000:1000 -q -n 0:500:200:1 -r 100:500'
+# Ejecutar bonnie++ y redirigir la salida al archivo personalizado
+subprocess.run(["bonnie++", "-d", "/mnt/md0", "-u", "raid", "-s", "0", "-r", "0", "-b", "-n", "0", "-m", "raid", "-f", output_file], stdout=subprocess.PIPE)
 
-    # Ejecutar el comando y capturar la salida
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-
-    # Guardar los resultados en un archivo de texto
-    with open(benchmark_output, 'w') as file:
-        file.write(stdout.decode())
-
-    # Imprimir la salida y el error (si los hay)
-    print('Salida:')
-    print(stdout.decode())
-    print('Error:')
-    print(stderr.decode())
-
-# Ejecutar la función para realizar el benchmark del RAID 0
-run_bonnie_raid0()
+print("Bonnie++ ejecutado correctamente.")
--- DD/scriptDD.sh
+++ DD/scriptDD.sh
@@ -6,17 +6,16 @@ tamanos_bloques=("1M" "2M" "4M")
 cantidades_bloques=("100" "200" "500")
 
 # Encabezado del archivo CSV
-echo "Tamaño Bloque, Cantidad Bloques, Tiempo, Temperatura" > $archivo_salida
+echo "Tamaño Bloque, Cantidad Bloques, Tiempo" > $archivo_salida
 
 # Bucle para ejecutar las pruebas con diferentes configuraciones
 for tamano_bloque in "${tamanos_bloques[@]}"
 do
     for cantidad_bloques in "${cantidades_bloques[@]}"
     do
-        tiempo=$(sudo dd if=/dev/zero of=./archivo_salida bs=$tamano_bloque count=$cantidad_bloques 2>&1 | grep "elapsed" | awk '{print $NF}')
-        temperatura=$(sensors | grep "RAID Temperature" | awk '{print $3}')
-        echo "$tamano_bloque, $cantidad_bloques, $tiempo, $temperatura"
-        echo "$tamano_bloque, $cantidad_bloques, $tiempo, $temperatura" >> $archivo_salida
+        tiempo=$(sudo dd if=/dev/zero of=/mnt/md1/archivo_salida  bs=$tamano_bloque count=$cantidad_bloques)
+        echo "$tamano_bloque, $cantidad_bloques, $tiempo"
+        echo "$tamano_bloque, $cantidad_bloques, $tiempo" > $archivo_salida
     done
 done
 sudo dd if=/dev/zero of=./archivo_salida bs=1M count=100
